0688b8d8a4ac06b498a94b45d01e9031a05eb985
Thiago da Silva Pinto <email.thiagosp@gmail.com>
2020-03-28 10:24:03 -0300

    Implementação das validações no backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routers = require('./routers');
 
 const app = express();
@@ -7,5 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routers);
+app.use(errors());
 
 app.listen(3333);
